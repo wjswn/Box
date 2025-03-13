0188ae15b64ad75bc30cce4fa69a02f449cc84f6
wjswn <wjswn9420@icloud.com>
2025-03-14 00:29:31 +0800

    更新 Box.js

--- Box.js
+++ Box.js
@@ -1,10 +1,10 @@
 [
     {
-       "标题": "作者QQ",
+       "bt": "作者QQ",
         "url": "https://qm.qq.com/q/2N4tGz0VXy"
     },
     {
-        "标题": "QQ群",
+        "bt": "QQ群",
         "url": "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=deC2vsgH53lopCnpfMZvSN05wvFRigXW&authKey=u8yg1IIAbbdQCyd9l6PBnaYIZ2Ro6pVBNOMoRBVgdG2KugDeBur3qJkrF0P7Vjw1&noverify=0&group_code=483062522"
     }
 ]
