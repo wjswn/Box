1a67153400c9beb4409f1315034fd063fed086fa
wjswn <wjswn9420@icloud.com>
2025-03-16 15:58:39 +0800

    更新 Box.js

--- Box.js
+++ Box.js
@@ -4,11 +4,11 @@
         "url": "https://qm.qq.com/q/2N4tGz0VXy"
     },
     {
-        "bt": "QQ群",
+        "bt": "C#刘陈鬼 QQ群",
         "url": "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=deC2vsgH53lopCnpfMZvSN05wvFRigXW&authKey=u8yg1IIAbbdQCyd9l6PBnaYIZ2Ro6pVBNOMoRBVgdG2KugDeBur3qJkrF0P7Vjw1&noverify=0&group_code=483062522"
     },
     {
-        "bt": "QQ群",
-        "url": "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=deC2vsgH53lopCnpfMZvSN05wvFRigXW&authKey=u8yg1IIAbbdQCyd9l6PBnaYIZ2Ro6pVBNOMoRBVgdG2KugDeBur3qJkrF0P7Vjw1&noverify=0&group_code=483062522"
+        "bt": "共享吧Box1.0 QQ群",
+        "url": "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=uJHXDlSW-poT4zynPwUb0Nxugk6qNYDP&authKey=zZ%2BpvBnHj3Uga4YGMHJdoQSW7NSdqLEpY1WsH1U5jkemhjEMusw3Qwl2iSWmxKac&noverify=0&group_code=821710263"
     }
 ]
