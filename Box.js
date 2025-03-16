a98cbd894051b8e7603d7dc3b9a33fd543cecc55
wjswn <wjswn9420@icloud.com>
2025-03-16 16:13:04 +0800

    Update Box.js

--- Box.js
+++ Box.js
@@ -8,7 +8,7 @@
         "url": "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=deC2vsgH53lopCnpfMZvSN05wvFRigXW&authKey=u8yg1IIAbbdQCyd9l6PBnaYIZ2Ro6pVBNOMoRBVgdG2KugDeBur3qJkrF0P7Vjw1&noverify=0&group_code=483062522"
     },
     {
-        "bt": "共享吧Box1.0 QQ群",
+        "bt": "共享吧Box1.0 Q群",
         "url": "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=uJHXDlSW-poT4zynPwUb0Nxugk6qNYDP&authKey=zZ%2BpvBnHj3Uga4YGMHJdoQSW7NSdqLEpY1WsH1U5jkemhjEMusw3Qwl2iSWmxKac&noverify=0&group_code=821710263"
     }
 ]
